44af33453dff5e2bf0647f15a0c7a823aeb489f8
Ola <ola@olavea.com>
2024-09-04 15:46:48 +0200

    AAAnd now! I'm using tailwind again!

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,5 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Ola Vea';
-export const SITE_DESCRIPTION = 'Welcome to my blog!';
+export const SITE_DESCRIPTION = 'Welcome aboard!';
+
