946f68490c93fc02b01658c2c62671b58eca03e0
Ola <ola@olavea.com>
2024-09-23 16:26:30 +0200

    newsletter

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,18 @@ import tailwind from "@astrojs/tailwind";
 
 import sitemap from "@astrojs/sitemap";
 
+import netlify from "@astrojs/netlify";
+
 // contentCollectionCache: true,
 // https://astro.build/config
 export default defineConfig({
   site: "https://hal4.netlify.app/",
   integrations: [tailwind(), sitemap()],
+
   experimental: {
     contentLayer: true,
   },
+
+  output: "hybrid",
+  adapter: netlify(),
 });
